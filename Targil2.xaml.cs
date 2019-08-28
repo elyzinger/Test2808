c4ea60af17fbc05a96fe3d0db5d9119246169c72
elyzinger <45950112+elyzinger@users.noreply.github.com>
2019-08-28 21:03:10 +0300

    Add files via upload

---
+++ Targil2.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TEST2808
+{
+    /// <summary>
+    /// Interaction logic for Targil2.xaml
+    /// </summary>
+    public partial class Targil2 : Window
+    {
+        public Targil2()
+        {
+            InitializeComponent();
+        }
+        //private async void startBtn_Click(object sender, RoutedEventArgs e)
+        //{
+        //    for (int i = 50; i >= 0; i--)
+        //    {
+        //        counter.Text = i.ToString();
+        //        await Task.Run(() => { Thread.Sleep(250); });
+        //    }
+        //}
+    }
+}
---
+++ ViewModelWeb.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST2808
+{
+    class ViewModelWeb : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    static public string FormatBytes(long bytes)
+    {
+        string[] magnitudes =
+            new string[] { "GB", "MB", "KB", "Bytes" };
+        long max =
+            (long)Math.Pow(1024, magnitudes.Length);
+
+        return string.Format("{1:##.##} {0}",
+            magnitudes.FirstOrDefault(
+                magnitude =>
+                bytes > (max /= 1024)) ?? "0 Bytes",
+          (decimal)bytes / (decimal)max);
+    }
+}
